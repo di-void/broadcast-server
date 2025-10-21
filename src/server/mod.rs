14429152296829ef85aa675d897b81fb708b36cb
DEE <donzydonathan@gmail.com>
2025-10-21 12:51:39 +0100

    chore: slignt cleanup

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -45,25 +45,25 @@ pub async fn start() -> io::Result<()> {
             let msg = rx.recv().await.unwrap();
 
             match msg {
-                ChannelMessage::Text(client_id, msg) => {
+                ChannelMessage::Text(cid, msg) => {
                     let mut reg_lock_guard = reg_lock.lock().await;
                     println!("Broker has acquired client registry lock!");
 
                     for (_, client) in reg_lock_guard.iter_mut() {
-                        if client.id != client_id {
+                        if client.id != cid {
                             println!("Sending to client {}", client.id);
                             client.sender.send(msg.clone()).await.unwrap();
                             println!("Sent!");
                         }
                     }
                 }
-                ChannelMessage::Close(client_id) => {
+                ChannelMessage::Close(cid) => {
                     let mut tasks_table_guard = tasks_lock.lock().await;
                     println!("Broker has acquired tasks table lock!");
 
-                    let task = tasks_table_guard.remove(&client_id).unwrap();
+                    let task = tasks_table_guard.remove(&cid).unwrap();
                     task.cancel().await; // could deadlock?
-                    println!("Main handler task for client {client_id} has been cancelled");
+                    println!("Main handler task for client {cid} has been cancelled");
                 }
             }
         }
@@ -77,18 +77,18 @@ pub async fn start() -> io::Result<()> {
         println!("Starting websocket session!");
         let ws_stream = accept_async(stream).await.unwrap();
         let (mut ws_sender, mut ws_receiver) = ws_stream.split();
-        let client_id = {
+        let cid = {
             cc += 1;
             cc
         };
         let (b_tx, b_rx) = channel::bounded(2);
         let new_connection = Client {
-            id: client_id,
+            id: cid,
             sender: b_tx,
         };
 
         let mut registry_writer = registry_lock.lock().await;
-        registry_writer.insert(client_id, new_connection);
+        registry_writer.insert(cid, new_connection);
         drop(registry_writer); // release write lock
 
         let sender = tx.clone();
@@ -103,9 +103,7 @@ pub async fn start() -> io::Result<()> {
 
         let reg_lock2 = Arc::clone(&registry_lock);
         let main_handler = smol::spawn(async move {
-            println!("Client {client_id} has spawned into action");
-
-            let c_id = client_id;
+            println!("Client {cid} has spawned into action");
             let tx = sender;
 
             loop {
@@ -114,16 +112,16 @@ pub async fn start() -> io::Result<()> {
                     match res {
                         Ok(msg) => {
                             if msg.is_text() {
-                                let channel_msg = ChannelMessage::Text(c_id.clone(), msg);
+                                let channel_msg = ChannelMessage::Text(cid, msg);
                                 tx.send_blocking(channel_msg).unwrap(); // deadlock?
                             } else if msg.is_close() {
                                 let mut w = reg_lock2.lock().await;
-                                let _ = w.remove(&c_id); // remove client from registry
+                                let _ = w.remove(&cid); // remove client from registry
                                 bcast_handler_task.cancel().await; // clean up
                                 println!(
-                                    "Broadcast handler task for client {c_id} has been cancelled"
+                                    "Broadcast handler task for client {cid} has been cancelled"
                                 );
-                                tx.send_blocking(ChannelMessage::Close(c_id)).unwrap();
+                                tx.send_blocking(ChannelMessage::Close(cid)).unwrap();
                                 break;
                             }
                         }
@@ -131,10 +129,10 @@ pub async fn start() -> io::Result<()> {
                             println!("Something went wrong: {:?}", e);
 
                             let mut w = reg_lock2.lock().await;
-                            let _ = w.remove(&c_id);
+                            let _ = w.remove(&cid);
                             bcast_handler_task.cancel().await; // clean up
-                            println!("Broadcast handler task for client {c_id} has been cancelled");
-                            tx.send_blocking(ChannelMessage::Close(c_id)).unwrap();
+                            println!("Broadcast handler task for client {cid} has been cancelled");
+                            tx.send_blocking(ChannelMessage::Close(cid)).unwrap();
                             break;
                         }
                     }
@@ -143,7 +141,7 @@ pub async fn start() -> io::Result<()> {
         });
 
         let mut tasks_table_guard = tasks_table_lock.lock().await;
-        tasks_table_guard.insert(client_id, main_handler);
+        tasks_table_guard.insert(cid, main_handler);
     }
 
     Ok(())
